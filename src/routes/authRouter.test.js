c0cc1a761a035b68922cde7910d3594f40cf4fe2
GuernJaq <guernjaq000@gmail.com>
2024-09-30 23:14:38 -0600

    #d3 properly set up admin generation

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,16 +2,38 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+const testUser2 = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 const emptyUser = {};
-const initUser = { name : '常用名字', email: 'a@jwt.com', password: 'admin'}
+//const initUser = { name : '常用名字', email: 'a@jwt.com', password: 'admin'}
+let adminUser;
 let adminUserAuthtoken;
 let testUserAuthToken;
 
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = Math.random().toString(36).substring(2, 12);
+    user.email = user.name + '@admin.com';
+
+    await DB.addUser(user);
+
+    user.password = 'toomanysecrets';
+    return user;
+}
+
 beforeAll(async () => {
+    adminUser = await createAdminUser()
+
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
-    const loginRes = await request(app).put('/api/auth').send(initUser);
+
+    testUser2.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const registerRes2 = await request(app).post('/api/auth').send(testUser2);
+    console.log(registerRes2.body.token)
+
+    const loginRes = await request(app).put('/api/auth').send(adminUser);
     adminUserAuthtoken = loginRes.body.token
 });
 
@@ -42,8 +64,7 @@ test('failed calls', async () => {
 });
 
 test('update user', async () => {
-    const updateRes = await request(app).put('/api/auth/1').set('Authorization', `Bearer ${adminUserAuthtoken}`).send(initUser);
+    const updateRes = await request(app).put('/api/auth/3').set('Authorization', `Bearer ${adminUserAuthtoken}`).send(adminUser);
     expect(updateRes.status).toBe(200)
-    expect(updateRes.body.email).toBe(initUser.email)
-    expect(updateRes.body.name).toBe(initUser.name)
+    expect(updateRes.body.email).toBe(adminUser.email)
 })
